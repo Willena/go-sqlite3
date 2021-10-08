471f57ddcdd6f92b7dbe95eeb57adf43dfa14437
Guillaume Villena <guillaume@villena.me>
2021-10-09 01:31:59 +0200

    Updated *.c *.h with content from SQLiteMC 1.3.4 (SQlite 3.36)

--- sqlite3-binding.h
+++ sqlite3-binding.h
@@ -1,5 +1,19 @@
 #ifndef USE_LIBSQLITE3
 /*
+** Name:        sqlite3mc.h
+** Purpose:     Header file for SQLite3 Multiple Ciphers support
+** Author:      Ulrich Telle
+** Created:     2020-03-01
+** Copyright:   (c) 2019-2020 Ulrich Telle
+** License:     MIT
+*/
+
+#ifndef SQLITE3MC_H_
+#define SQLITE3MC_H_
+
+/* #include "sqlite3.h" */
+/*** Begin of #include "sqlite3.h" ***/
+/*
 ** 2001-09-15
 **
 ** The author disclaims copyright to this source code.  In place of
@@ -12352,10 +12366,12 @@ struct fts5_api {
 #endif /* _FTS5_H */
 
 /******** End of fts5.h *********/
-#else // USE_LIBSQLITE3
- // If users really want to link against the system sqlite3 we
-// need to make this file a noop.
- #endif
+/*** End of #include "sqlite3.h" ***/
+
+
+#ifdef SQLITE_USER_AUTHENTICATION
+/* #include "sqlite3userauth.h" */
+/*** Begin of #include "sqlite3userauth.h" ***/
 /*
 ** 2014-09-08
 **
@@ -12452,3 +12468,198 @@ int sqlite3_user_delete(
 #endif
 
 #endif /* SQLITE_USER_AUTHENTICATION */
+/*** End of #include "sqlite3userauth.h" ***/
+
+#endif
+
+/*
+** Symbols for ciphers
+*/
+#define CODEC_TYPE_UNKNOWN   0
+#define CODEC_TYPE_AES128    1
+#define CODEC_TYPE_AES256    2
+#define CODEC_TYPE_CHACHA20  3
+#define CODEC_TYPE_SQLCIPHER 4
+#define CODEC_TYPE_RC4       5
+#define CODEC_TYPE_MAX       5
+
+/*
+** Definitions of supported ciphers
+*/
+
+/*
+** Compatibility with wxSQLite3
+*/
+#ifdef WXSQLITE3_HAVE_CIPHER_AES_128_CBC
+#define HAVE_CIPHER_AES_128_CBC WXSQLITE3_HAVE_CIPHER_AES_128_CBC
+#endif
+
+#ifdef WXSQLITE3_HAVE_CIPHER_AES_256_CBC
+#define HAVE_CIPHER_AES_256_CBC WXSQLITE3_HAVE_CIPHER_AES_256_CBC
+#endif
+
+#ifdef WXSQLITE3_HAVE_CIPHER_CHACHA20
+#define HAVE_CIPHER_CHACHA20 WXSQLITE3_HAVE_CIPHER_CHACHA20
+#endif
+
+#ifdef WXSQLITE3_HAVE_CIPHER_SQLCIPHER
+#define HAVE_CIPHER_SQLCIPHER WXSQLITE3_HAVE_CIPHER_SQLCIPHER
+#endif
+
+#ifdef WXSQLITE3_HAVE_CIPHER_RC4
+#define HAVE_CIPHER_RC4 WXSQLITE3_HAVE_CIPHER_RC4
+#endif
+
+/*
+** Actual definitions of supported ciphers
+*/
+#ifndef HAVE_CIPHER_AES_128_CBC
+#define HAVE_CIPHER_AES_128_CBC 1
+#endif
+
+#ifndef HAVE_CIPHER_AES_256_CBC
+#define HAVE_CIPHER_AES_256_CBC 1
+#endif
+
+#ifndef HAVE_CIPHER_CHACHA20
+#define HAVE_CIPHER_CHACHA20 1
+#endif
+
+#ifndef HAVE_CIPHER_SQLCIPHER
+#define HAVE_CIPHER_SQLCIPHER 1
+#endif
+
+#ifndef HAVE_CIPHER_RC4
+#define HAVE_CIPHER_RC4 1
+#endif
+
+/*
+** Check that at least one cipher is be supported
+*/
+#if HAVE_CIPHER_AES_128_CBC == 0 &&  \
+    HAVE_CIPHER_AES_256_CBC == 0 &&  \
+    HAVE_CIPHER_CHACHA20    == 0 &&  \
+    HAVE_CIPHER_SQLCIPHER   == 0 &&  \
+    HAVE_CIPHER_RC4         == 0
+#error Enable at least one cipher scheme!
+#endif
+
+/*
+** Definition of API functions
+*/
+
+/*
+** Define Windows specific SQLite API functions (not defined in sqlite3.h)
+*/
+#if SQLITE_OS_WIN == 1
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+SQLITE_API int sqlite3_win32_set_directory(unsigned long type, void* zValue);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*
+** Specify the key for an encrypted database.
+** This routine should be called right after sqlite3_open().
+**
+** Arguments:
+**   db       - Database to be encrypted
+**   zDbName  - Name of the database (e.g. "main")
+**   pKey     - Passphrase
+**   nKey     - Length of passphrase
+*/
+SQLITE_API int sqlite3_key(sqlite3* db, const void* pKey, int nKey);
+SQLITE_API int sqlite3_key_v2(sqlite3* db, const char* zDbName, const void* pKey, int nKey);
+
+/*
+** Change the key on an open database.
+** If the current database is not encrypted, this routine will encrypt
+** it.  If pNew==0 or nNew==0, the database is decrypted.
+**
+** Arguments:
+**   db       - Database to be encrypted
+**   zDbName  - Name of the database (e.g. "main")
+**   pKey     - Passphrase
+**   nKey     - Length of passphrase
+*/
+SQLITE_API int sqlite3_rekey(sqlite3* db, const void* pKey, int nKey);
+SQLITE_API int sqlite3_rekey_v2(sqlite3* db, const char* zDbName, const void* pKey, int nKey);
+
+/*
+** Specify the activation key for a SEE database.
+** Unless activated, none of the SEE routines will work.
+**
+** Arguments:
+**   zPassPhrase  - Activation phrase
+**
+** Note: Provided only for API compatibility with SEE.
+** Encryption support of SQLite3 Multi Cipher is always enabled.
+*/
+SQLITE_API void sqlite3_activate_see(const char* zPassPhrase);
+
+/*
+** Define functions for the configuration of the wxSQLite3 encryption extension
+*/
+SQLITE_API int sqlite3mc_config(sqlite3* db, const char* paramName, int newValue);
+SQLITE_API int sqlite3mc_config_cipher(sqlite3* db, const char* cipherName, const char* paramName, int newValue);
+SQLITE_API unsigned char* sqlite3mc_codec_data(sqlite3* db, const char* zDbName, const char* paramName);
+SQLITE_API const char* sqlite3mc_version();
+
+#ifdef SQLITE3MC_WXSQLITE3_COMPATIBLE
+SQLITE_API int wxsqlite3_config(sqlite3* db, const char* paramName, int newValue);
+SQLITE_API int wxsqlite3_config_cipher(sqlite3* db, const char* cipherName, const char* paramName, int newValue);
+SQLITE_API unsigned char* wxsqlite3_codec_data(sqlite3* db, const char* zDbName, const char* paramName);
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+/*
+** Define public SQLite3 Multiple Ciphers VFS interface
+*/
+/* #include "sqlite3mc_vfs.h" */
+/*** Begin of #include "sqlite3mc_vfs.h" ***/
+/*
+** Name:        sqlite3mc_vfs.h
+** Purpose:     Header file for VFS of SQLite3 Multiple Ciphers support
+** Author:      Ulrich Telle
+** Created:     2020-03-01
+** Copyright:   (c) 2020 Ulrich Telle
+** License:     MIT
+*/
+
+#ifndef SQLITE3MC_VFS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+SQLITE_API int sqlite3mc_vfs_create(const char* zVfsReal, int makeDefault);
+SQLITE_API void sqlite3mc_vfs_destroy(const char* zName);
+SQLITE_API void sqlite3mc_vfs_shutdown();
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SQLITE3MC_VFS_H_ */
+/*** End of #include "sqlite3mc_vfs.h" ***/
+
+
+#endif
+#else // USE_LIBSQLITE3
+ // If users really want to link against the system sqlite3 we
+// need to make this file a noop.
+ #endif
